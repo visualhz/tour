3f1b91c0266b9f8b545ae57c9a49b3d62e6b98ae
练林明 <394229081@qq.com>
2019-08-04 08:43:23 +0800

    项目结构完善--by LLM 2019-08-04

--- src/main/java/com/gxu/tour/TourApplication.java
+++ src/main/java/com/gxu/tour/TourApplication.java
@@ -1,9 +1,13 @@
 package com.gxu.tour;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
+//扫描 mybatis mapper 包路径
+@MapperScan(basePackages = "com.gxu.tour.mapper")
 public class TourApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/gxu/tour/TourApplicationTests.java
+++ src/test/java/com/gxu/tour/TourApplicationTests.java
@@ -1,16 +1,18 @@
 package com.gxu.tour;
 
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TourApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
+
 
 }
