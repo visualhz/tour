bcfeaab5861a720b6f33961e87ed16c244736d50
zqw961025 <2384236461@qq.DOM>
2019-09-11 19:28:20 +0800

    根据用户ID返回数据修改

--- src/main/java/com/gxu/tour/controller/LfmController.java
+++ src/main/java/com/gxu/tour/controller/LfmController.java
@@ -1,16 +1,21 @@
 package com.gxu.tour.controller;
 
+import com.gxu.tour.entity.Route;
 import com.gxu.tour.service.LfmService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @ClassName LfmController
@@ -43,14 +48,27 @@ public class LfmController {
         return  null;
     }
 
-    //获取10个路线组合的推荐产品,根据用户ID
+    /**
+     * 获取10个路线组合的推荐产品,根据用户ID
+     * @param userID 用户名
+     * @param request
+     * @return
+     */
     @GetMapping("/GetRecDataOfRoute")
     @ResponseBody
-    public Object GetRecDataOfRoute(HttpServletRequest request, HttpServletResponse response, Model model)
+    public Object GetRecDataOfRoute(@RequestParam(value = "userID") String userID, HttpServletRequest request, HttpServletResponse response, Model model)
     {
 //        String userID=request.getParameter("userID");
-        String userID="103404";
-        return  lfmService.getRouteRecID(userID);
+//        String userID="103404";
+//        return  lfmService.getRouteRecID(userID);
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        String[] productId = lfmService.getRouteRecID(userID);
+        List<Route> routes = lfmService.getRouteRecById(productId);
+        Map<String,List<Route>> recommendRotesMap = new HashMap<>();
+        recommendRotesMap.put("routes",routes);
+        return  recommendRotesMap;
     }
 
     //获取10个路线组合的推荐产品,根据热度推荐
